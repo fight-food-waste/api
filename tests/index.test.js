2f23c2d051c0c0870088e9bc900da2a5b5e1cf18
angristan <angristan@pm.me>
2019-04-07 22:58:47 +0200

    Improve tests: fix requires, remove chai, use assert

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,21 +1,20 @@
-/* eslint-disable no-undef */
 const request = require('supertest');
+const { describe, it } = require('mocha');
+const assert = require('assert');
 
-// eslint-disable-next-line no-unused-vars
-const should = require('chai').should();
 const app = require('../app');
 
 describe('GET /', () => {
     it('the status code should be 200', async () => {
         const response = await request(app).get('/');
-        response.statusCode.should.equal(200);
+        assert.strictEqual(response.statusCode, 200);
     });
     it('the content type should be JSON', async () => {
         const response = await request(app).get('/');
-        response.headers['content-type'].should.equal('application/json; charset=utf-8');
+        assert.strictEqual(response.headers['content-type'], 'application/json; charset=utf-8');
     });
     it('the response should be a welcome message', async () => {
         const response = await request(app).get('/');
-        response.text.should.equal(JSON.stringify({ message: 'Welcome to the FFW API!' }));
+        assert.strictEqual(response.text, JSON.stringify({ message: 'Welcome to the FFW API!' }));
     });
 });
