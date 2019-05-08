b2d62748f1e9d3f513a5f391d17f541697a26864
angristan <angristan@pm.me>
2019-05-09 00:45:13 +0200

    auth: handle non-existant users

--- controllers/auth.js
+++ controllers/auth.js
@@ -27,33 +27,37 @@ const authController = {
           .table('donors')
           .where('email', value.email)
           .then((rows) => {
-            const donor = rows[0];
-            const hashedPassword = donor.password;
-            const receivedPassword = value.password;
+            if (rows.length === 0) {
+              res.sendStatus(400);
+            } else {
+              const donor = rows[0];
+              const hashedPassword = donor.password;
+              const receivedPassword = value.password;
 
-            bcrypt.compare(receivedPassword, hashedPassword)
-              .then((passwordIsValid) => {
-                if (passwordIsValid) {
-                  const userToken = token.generate();
+              bcrypt.compare(receivedPassword, hashedPassword)
+                .then((passwordIsValid) => {
+                  if (passwordIsValid) {
+                    const userToken = token.generate();
 
-                  knex('user_tokens')
-                    .insert({
-                      user_id: donor.id,
-                      token: userToken,
-                      date: new Date(),
-                    })
-                    .then(() => {
-                      res.send({ token: userToken });
-                    })
-                    .catch((error) => {
-                      console.log(error);
+                    knex('user_tokens')
+                      .insert({
+                        user_id: donor.id,
+                        token: userToken,
+                        date: new Date(),
+                      })
+                      .then(() => {
+                        res.send({ token: userToken });
+                      })
+                      .catch((error) => {
+                        console.log(error);
 
-                      res.sendStatus(500);
-                    });
-                } else {
-                  res.sendStatus(403);
-                }
-              });
+                        res.sendStatus(500);
+                      });
+                  } else {
+                    res.sendStatus(403);
+                  }
+                });
+            }
           })
           .catch((error) => {
             console.log(error);
