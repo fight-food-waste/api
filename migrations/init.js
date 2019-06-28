f3cbb3cdbeca98479fb92557e6330ee0cd41ec70
angristan <angristan@pm.me>
2019-06-28 02:20:26 +0200

    Harmonize tables with Laravel projet

--- migrations/init.js
+++ migrations/init.js
@@ -1,32 +1,24 @@
 exports.up = (knex, Promise) => Promise.all([
-  knex.schema.createTable('donors', (table) => {
+  knex.schema.createTable('users', (table) => {
     table.increments();
-    table.timestamps();
     table.string('first_name', 60)
       .notNullable();
-    table.string('middle_name', 60);
     table.string('last_name', 60)
       .notNullable();
     table.string('email')
       .notNullable()
       .unique();
-    table.string('company_name', 100);
-    table.string('phone_number', 60);
     table.string('password')
       .notNullable();
   }),
   knex.schema.createTable('bundles', (table) => {
     table.increments();
-    table.integer('donor_id')
+    table.integer('user_id')
       .unsigned()
       .references('id')
-      .inTable('donors');
+      .inTable('users');
     table.dateTime('submitted_at');
-    // table.date('validated_at');
-    // table.integer('bundle_status_id');
-    // table.integer('distribution_round_id');
-    // table.integer('collection_round_id');
-    table.string('status')
+    table.integer('status')
       .defaultTo('open');
   }),
   knex.schema.createTable('products_scanned', (table) => {
@@ -50,7 +42,7 @@ exports.up = (knex, Promise) => Promise.all([
     table.integer('user_id')
       .unsigned()
       .references('id')
-      .inTable('donors');
+      .inTable('users');
     table.dateTime('date');
   }),
 ]);
@@ -59,5 +51,5 @@ exports.down = (knex, Promise) => Promise.all([
   knex.schema.dropTable('products_scanned'),
   knex.schema.dropTable('bundles'),
   knex.schema.dropTable('user_tokens'),
-  knex.schema.dropTable('donors'),
+  knex.schema.dropTable('users'),
 ]);
